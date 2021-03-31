62372da03f075ea170ccb79f43763aa9a2d18bcd
wimiros <wicinskimiroslaw@student.kul.lublin.pl>
2021-03-31 18:32:39 +0200

    My second commit MW

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("TEST_MW");
+        System.out.println("Mój drugi commit_MW");
 	// write your code here
     }
 }
