1e0cb2a8a33a2021a0a4a02a338fe637036af963
wimiros <wicinskimiroslaw@student.kul.lublin.pl>
2021-03-31 19:19:16 +0200

    My third commit MW

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("TEST_MW");
         System.out.println("Mój drugi commit_MW");
+        System.out.println("Kolejny commit_MW");
 	// write your code here
     }
 }
